bd30c0126e62cc35f7dd785453b7ba001a330844
Vlad Pisarenko <vladpisarenko2126@gmail.com>
2025-07-21 12:41:38 +0300

    fix bugs and typos

--- Shopping_cart_analis.py
+++ Shopping_cart_analis.py
@@ -7,28 +7,32 @@ raw_data = [
 ]
 
 def log_maker(function):
-    def inner_funktion(*args, **kwargs):
+    def inner_function(*args, **kwargs):
         print(f"function {function.__name__} in progress")
         result = function(*args, **kwargs)
         print(f"function {function.__name__} is completed")
         return result
-    return inner_funktion
+    return inner_function
 
 @log_maker
 def dict_former(lists = raw_data):
-    separeited_list = list(item.split(":") for item in lists)
-    new_dict = {item[0]:int(item[1]) for item in separeited_list}
+    separated_list = list(item.split(":") for item in lists)
+    new_dict = {key:int(value) for key,value in separated_list}
     print(new_dict)
     return new_dict
 
 @log_maker
-def price_filter(dictionary = dict_former(),price = 100):
+def price_filter(dictionary = None,price = 100):
+    if dictionary == None:
+        dictionary = dict_former()
     filtered_dict = {key:value for key,value in dictionary.items() if value>price}
     print(filtered_dict)
     return filtered_dict
 
 @log_maker
-def customer_list(dictionary = price_filter()):
+def customer_list(dictionary = None):
+    if dictionary == None:
+        dictionary = price_filter()
     new_list = [f"{key} стоит {value}$" for key,value in dictionary.items()]
     print(new_list)
     return new_list
