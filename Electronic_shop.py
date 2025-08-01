4e9831799b50111879f3ea119ed4fb15df920fa1
Vlad Pisarenko <vladpisarenko2126@gmail.com>
2025-08-01 14:21:47 +0300

    add methods in User class

--- Electronic_shop.py
+++ Electronic_shop.py
@@ -14,11 +14,25 @@ class Product:
     def __eq__(self, value):
         return value == self.name
     
-class Cart(Product):        
+class Cart():        
     pass
 
 class User:
-    pass
+    def __init__(self,name,balance):
+        self.name = name
+        self._balance = int(balance)
+        self.card = None
+    
+    def __str__(self):
+        return f"User name: {self.name}, balaance: {self._balance}$"
+    
+    def __gt__(self,other):
+        if self._balance > other._balance:
+            return f"{self.name}'s balance is greater then {other.name}"
+        elif self._balance == other._balance:
+            return f"{self.name}'s balance is equal then {other.name}"
+        else:
+            return f"{self.name}'s balance is less then {other.name}"
             
 p1 = Product("iPhone 15", "smartphone", 1200)
 p2 = Product("MacBook Pro", "laptop", 2500)
